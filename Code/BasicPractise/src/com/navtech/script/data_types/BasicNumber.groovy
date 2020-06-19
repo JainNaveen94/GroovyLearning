bd2c0d96d7b344088627a7784e61a45b709a75da
Naveen Jain <47914199+JainNaveen94@users.noreply.github.com>
2020-06-19 13:07:48 +0530

    basic operator and Number type Done

---
+++ Code/BasicPractise/src/com/navtech/script/data_types/BasicNumber.groovy
@@ -0,0 +1,24 @@
+package com.navtech.script.data_types
+
+// Integer Type which is not primitive instead Integer Wrapper Type
+println 10.class
+
+// Integer Type which is not primitive instead Integer Wrapper Type
+int x = 10
+println x.class
+
+// BigInteger Type or BigLong
+def a = 12345678900987654321
+println a.class
+
+// Long Type
+def b = 1234567890123
+println b.class
+
+// BigDecimal Type
+def c = 12.34
+println c.getClass().getName()
+
+// Double Type
+def d = 12.34d
+println d.class
---
+++ Code/BasicPractise/src/com/navtech/script/operators/SafeNavigationOperatorDemo.groovy
@@ -0,0 +1,22 @@
+package com.navtech.script.operators
+
+class Person {
+    String name
+}
+
+// Find will return null to the person object
+//def person = null
+//def person = Person.find()
+def person = new Person()
+
+// Printing Person Object person on console
+println person
+
+// here, safe navigation operator is used to check object is null or not before accessing is properties and value
+def name = person?.name
+
+// Printing Person Object propertie value
+println "$name"
+
+// Check that property is null or not through assertion
+assert name == null
